86deded7a668530d44c850fe07bfb3b3658aada9
Zachary Kolker <zkolker@enova.com>
2024-03-05 16:06:30 -0500

    added specs to teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
+  validates :region, :country, :full_name, presence: true
   belongs_to :league
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Team do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:full_name) }
+    it { is_expected.to validate_presence_of(:region) }
+    it { is_expected.to validate_presence_of(:country) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:league).class_name('League') }
+  end
 end
