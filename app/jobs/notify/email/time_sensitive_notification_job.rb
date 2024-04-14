1e9a577e1601d4ddb6f17ae265e47f253bc06903
Kalmai <leozverres@gmail.com>
2024-04-14 02:16:34 -0400

    should be able to email at a time and we can now find the time to send it

--- app/jobs/notify/email/time_sensitive_notification_job.rb
+++ app/jobs/notify/email/time_sensitive_notification_job.rb
@@ -6,6 +6,8 @@ module Notify
       queue_as :high_priority
 
       def perform
+        # TODO: should look into finding users this way
+        # users = User.joins(:promotions).where.not('promotions.timing_methods': nil)
         Promotion.where.not(timing_methods: nil).each do |promotion|
           notification_time = promotion.utc_notification_time
           next unless notification_time
@@ -15,6 +17,14 @@ module Notify
             email = user.contact_methods&.first&.contact_detail
             next unless today_for_user?(notification_time, user.timezone)
 
+            # this is how we're going to send emails at a certain time
+            # RegistrationMailer.with(user:).welcome_email.deliver_later(wait_until: Time.zone.now + 1.minute)
+            # getting the time difference will be something like
+            # diff = Time.zone.now - notification_time # this is in seconds apparently
+            # hrs = (diff.abs/60/60)
+            # RegistrationMailer.with(user:).welcome_email.deliver_later(wait_until: Time.zone.now + hrs)
+            # Time.at(Time.zone.now.in(hrs.hours))
+            # verify above and timezone by calling .zone on the Time object above
             puts "#{email}: #{notification_time}"
           end
         end
