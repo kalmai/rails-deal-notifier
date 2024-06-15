9d05877d1b68603e6b128b22aa9d9d7c519cb50f
Kalmai <leozverres@gmail.com>
2024-06-14 20:50:05 -0400

    add notification job for promos

---
+++ app/jobs/notify_actionable_promotion_job.rb
@@ -0,0 +1,7 @@
+class NotifyActionablePromotionJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ spec/jobs/notify_actionable_promotion_job_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe NotifyActionablePromotionJob, type: :job do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
