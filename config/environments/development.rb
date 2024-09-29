96ca12e9ddb66252142b86eef59253823e462358
kalmai <leozverres@gmail.com>
2024-09-28 21:48:56 -0400

    swap from good_job to solid_queue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,6 +80,10 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :development } }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
