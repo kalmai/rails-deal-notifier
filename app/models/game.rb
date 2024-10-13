eb159232c8050e1ba47349a5b91ba8cde407cc00
kalmai <leozverres@gmail.com>
2024-10-13 02:06:21 -0400

    began game evaluator module

--- app/models/game.rb
+++ app/models/game.rb
@@ -17,61 +17,6 @@ class Game < ApplicationRecord
     goals.where(team_id: away_team.id)
   end
 
-  def played?(timezone:)
-    @played ||= Time.use_zone(timezone) { utc_start_time.in_time_zone(timezone)&.yesterday? } || false
-  end
-
-  def scored_in?(timezone:, period:)
-    return false unless played?(timezone:) && goals.present?
-
-    goals.any? { _1.period == period.to_i }
-  end
-
-  def first_goal?
-    return false unless played?
-
-    defender = results_cache[@args[:short_name]]
-    opponent = results_cache[defender.opponent.downcase]
-    return false if defender.goals.empty?
-
-    scored_first_goal?(defender, opponent)
-  end
-
-  def scored_first_goal?(defender, attacker)
-    defender_first_goal = defender.goals.first
-    attacker_first_goal = attacker.goals.first
-    return true if defender_first_goal.period < attacker_first_goal.period
-    return false if defender_first_goal.period > attacker_first_goal.period
-
-    Time.parse("00:#{defender_first_goal.time}") < Time.parse("00:#{attacker_first_goal.time}")
-  end
-
-  def perfect_defence?
-    return false unless played?
-
-    defender = results_cache[@args[:short_name]]
-    opponent = results_cache[defender.opponent.downcase]
-    opponent.goals.empty?
-  end
-
-  def goal_count_equal_or_above?
-    return false unless played?
-
-    results_cache[@args[:short_name]].goals.size >= @args[:goals_count]
-  end
-
-  def home?
-    played? && results_cache[@args[:short_name]].try(:away?) == false
-  end
-
-  def away?
-    played? && results_cache[@args[:short_name]].try(:away?)
-  end
-
-  def won?
-    played? && results_cache[@args[:short_name]].try(:won?) == true
-  end
-
   class << self
     def most_recent_game(team_id:)
       find_games_for(team_id:).order(:utc_start_time).reverse_order.where(has_consumed_results: true).first
---
+++ lib/evaluator.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Evaluator
+  class Client
+    def initialize(timezone:, team_id:, game:)
+      @timezone = timezone
+      @team_id = team_id
+      @game = game
+    end
+
+    def home_goals
+      @game.home_goals
+    end
+
+    def away_goals
+      @game.game_goals
+    end
+
+    def played?
+      @played ||= Time.use_zone(@timezone) do
+        game.has_consumed_results && utc_start_time.in_time_zone(@timezone)&.yesterday?
+      end || false
+    end
+
+    def scored_in?(period:)
+      return false unless played? && @game.goals.present?
+
+      @game.goals.any? { _1.period == period.to_i }
+    end
+
+    def first_goal?
+      return false unless played?
+
+      @game.goals.first.team_id == @team_id
+    end
+
+    def perfect_defence?
+      return false unless played?
+
+      @game.away_goals.blank?
+    end
+
+    def goal_count_equal_or_above?
+      return false unless played?
+
+      @game.goals.where(team_id: @team_id)
+    end
+
+    def home?
+      @game.home_team_id == @team_id
+    end
+
+    def away?
+      @game.away_team_id == @team_id
+    end
+
+    def won?
+      played? && (@game.home_goals.count <=> @game.away_goals.count) == (away? ? -1 : 1)
+    end
+  end
+end
