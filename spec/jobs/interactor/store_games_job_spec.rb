04c1341b92177855ed712d8bc9a211e79c3676c1
kalmai <leozverres@gmail.com>
2024-11-11 17:34:31 -0500

    added store_games_job spec and updated league season_range to in_season?

--- spec/jobs/interactor/store_games_job_spec.rb
+++ spec/jobs/interactor/store_games_job_spec.rb
@@ -9,25 +9,34 @@ RSpec.describe Interactor::StoreGamesJob do
   let(:freeze_time) { Time.parse('2024-10-02T10:00:00.0000000Z') }
 
   before do
+    league = create(:league, end_month:, short_name: 'mls')
+    allow(League).to receive(:all).and_return([league])
     Timecop.freeze(freeze_time)
-    create(:league, end_month:, short_name: 'mls')
-    job.perform
   end
 
   after { Timecop.return }
 
   describe '#perform' do
+    before do
+      stub_request(:get, %r{#{Interactor::Mls::BASE_URL}/matches/*})
+        .to_return(status: 200, body: file_fixture('mls/raw_data.json').read)
+      create(:team, short_name: 'clb')
+      create(:team, short_name: 'ne')
+      create(:team, short_name: 'van')
+      create(:team, short_name: 'lafc')
+    end
+
     it 'performs the job successfully' do
-      expect { job }.not_to raise_error
-      # expect(RegistrationMailer).to have_received(:with).with({ user: })
+      expect { job.perform_now }.to change(Game, :count).by(2)
     end
 
     context 'when the league is out of season' do
       let(:end_month) { '9' }
+      let(:interactor) { Interactor::Mls }
 
       before do
-        allow(Interactor::Mls).to receive(:new).and_return(Interactor::Mls)
-        allow(Interactor::Mls).to receive(:store_games)
+        allow(Interactor::Mls).to receive(:new).and_return(interactor)
+        allow(interactor).to receive(:store_games)
       end
 
       it 'does not trigger the interactor' do
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -15,21 +15,33 @@ RSpec.describe League do
   end
 
   describe '#in_season?' do
-    subject(:range) { build(:league, end_month:).in_season? }
+    subject(:range) { build(:league, start_month:, end_month:, short_name: 'mls').in_season? }
 
+    let(:freeze_time) { Time.parse('2024-10-02T10:00:00.0000000Z') }
+    let(:start_month) { 1 }
     let(:end_month) { '11' }
 
-    it 'yields a valid range' do
-      expect(range.min).to eq(Time.new(Time.now.year, 1))
-      expect(range.max).to eq(Time.new(Time.now.year, 11).end_of_month)
+    before { Timecop.freeze(freeze_time) }
+    after { Timecop.return }
+
+    it { is_expected.to be true }
+
+    context 'when the end month falls outside of the season' do
+      let(:end_month) { '9' }
+
+      it { is_expected.to be false }
     end
 
     context 'when the end month is next year annotated by a `+` in the string' do
-      let(:end_month) { '11+' }
+      let(:start_month) { 9 }
+      let(:end_month) { '2+' }
 
-      it 'properly sets the max range to next year' do
-        expect(range.min).to eq(Time.new(Time.now.year, 1))
-        expect(range.max).to eq(Time.new(Time.now.next_year.year, 11).end_of_month)
+      it { is_expected.to be true }
+
+      context 'when the current date is not within the months of the season' do
+        let(:freeze_time) { Time.parse('2024-07-02T10:00:00.0000000Z') }
+
+        it { is_expected.to be false }
       end
     end
   end
