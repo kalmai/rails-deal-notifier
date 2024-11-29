37630a31d78edf616016622c6dd6932bd062ce82
kalmai <leozverres@gmail.com>
2024-11-29 00:58:14 -0500

    updated mailers and removed references to registration mailer which no longer exists

--- spec/jobs/notify/welcome_job_spec.rb
+++ spec/jobs/notify/welcome_job_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe Notify::WelcomeJob do
   describe '#perform' do
     before do
       # TODO: figure out how to fix this cop.
-      allow(RegistrationMailer).to receive_message_chain(:with, :welcome_email, :deliver_later).and_return(true) # rubocop:disable Rspec/MessageChain
+      allow(WelcomeMailer).to receive_message_chain(:with, :send_email, :deliver_later).and_return(true) # rubocop:disable Rspec/MessageChain
       job.perform(user:)
     end
 
     it 'performs the job successfully' do
       expect { job }.not_to raise_error
-      expect(RegistrationMailer).to have_received(:with).with({ user: })
+      expect(WelcomeMailer).to have_received(:with).with({ user: })
     end
   end
 end
