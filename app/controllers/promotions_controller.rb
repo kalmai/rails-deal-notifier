b01ef8bbf044c62440662c726abaa5838740bab4
kalmai <leozverres@gmail.com>
2025-06-24 00:04:42 -0400

    cleaned up promo controller show

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -7,18 +7,20 @@ class PromotionsController < ApplicationController
     # get all the teams that match the location and are included in the arr of in season leagues
     in_season_leagues = League.select(&:in_season?).map(&:id)
     teams = Team.where(league_id: in_season_leagues, country: location_data[:country], region: location_data[:region])
-    arr = []
-    teams.map do |team|
-      team.promotions.map do |promo|
-        Time.use_zone(location_data[:timezone]) do
-          arr.push(promo.attributes.with_indifferent_access) if promo.evaluate_most_recent_game
-        end
-      end
-    end
-    render json: arr
+    render json: promos_available_for_teams(teams, location_data[:timezone])
   end
 
   private
+
+  def promos_available_for_teams(teams, timezone)
+    teams.map do |team|
+      team.promotions.select do |promo|
+        Time.use_zone(timezone) do
+          promo.attributes.with_indifferent_access if promo.evaluate_most_recent_game
+        end
+      end
+    end
+  end
 
   def location_data(postal:)
     data = Geocoder.search(postal, params: { countrycodes: 'us,ca', type: :postcode }).first&.data&.dig('properties')
